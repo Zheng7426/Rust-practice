d2dbf887a43c379baf82190cc6cbc8a927bda702
Hz <36901033+Zheng7426@users.noreply.github.com>
2021-06-04 12:13:04 +0800

    cargo usage

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+/Users/hz7426/Documents/GitHub/rust-practice/projects/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
+
+/Users/hz7426/Documents/GitHub/rust-practice/projects/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/hz7426/Documents/GitHub/rust-practice/projects/hello_cargo/target/debug/hello_cargo: /Users/hz7426/Documents/GitHub/rust-practice/projects/hello_cargo/src/main.rs
