5c1a36ffc360fd4e6884f0dd514550107ded73d8
Hz <36901033+Zheng7426@users.noreply.github.com>
2021-06-07 13:10:19 +0800

    explore functions in Rust

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,10 +1,17 @@
-fn main() {
-    
-    println!("Hello, world!");
+use std::io;
 
-    another_function();
+
+fn main() {
+    let mut input = String::new();
+    println!("Hello, world!");
+    println!("Enter a number!");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input!");
+    let input: u32 = input.trim().parse().expect("Not good");
+    another_function(input);
 }
 
-fn another_function() {
-    println!("Yoyoyo.");
+fn another_function(x: u32) {
+    println!("Yo wasup! The value of x doubled is {}", x * 2);
 }
