b4d561e3b80b3497287bbecdf194ea2f58ae9cd9
Hz <36901033+Zheng7426@users.noreply.github.com>
2021-06-21 10:35:08 +0800

    separate modules

--- restaurant/target/debug/deps/restaurant-29702407c5420e16.d
+++ restaurant/target/debug/deps/restaurant-29702407c5420e16.d
@@ -1,5 +1,7 @@
-/Users/hz7426/Documents/GitHub/rust-practice/projects/restaurant/target/debug/deps/restaurant-29702407c5420e16.rmeta: src/lib.rs
+/Users/hz7426/Documents/GitHub/rust-practice/projects/restaurant/target/debug/deps/restaurant-29702407c5420e16.rmeta: src/lib.rs src/front_of_house.rs src/front_of_house/hosting.rs
 
-/Users/hz7426/Documents/GitHub/rust-practice/projects/restaurant/target/debug/deps/restaurant-29702407c5420e16.d: src/lib.rs
+/Users/hz7426/Documents/GitHub/rust-practice/projects/restaurant/target/debug/deps/restaurant-29702407c5420e16.d: src/lib.rs src/front_of_house.rs src/front_of_house/hosting.rs
 
 src/lib.rs:
+src/front_of_house.rs:
+src/front_of_house/hosting.rs:
