c8e743d491cd3c9a00b34eab28c60255727afa82
Hz <36901033+Zheng7426@users.noreply.github.com>
2021-06-21 10:16:42 +0800

    publicity

---
+++ restaurant/target/debug/deps/restaurant-29702407c5420e16.d
@@ -0,0 +1,5 @@
+/Users/hz7426/Documents/GitHub/rust-practice/projects/restaurant/target/debug/deps/restaurant-29702407c5420e16.rmeta: src/lib.rs
+
+/Users/hz7426/Documents/GitHub/rust-practice/projects/restaurant/target/debug/deps/restaurant-29702407c5420e16.d: src/lib.rs
+
+src/lib.rs:
