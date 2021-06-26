e88a6fec2616e0375cc74fd0d4e91edbd4c0d822
Hz <36901033+Zheng7426@users.noreply.github.com>
2021-06-26 09:25:07 +0800

    mode

--- collection_exercise/src/main.rs
+++ collection_exercise/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 
 fn average(numbers: &[i32]) -> f32 {
     numbers.iter().sum::<i32>() as f32 / numbers.len() as f32
@@ -13,4 +14,19 @@ fn main() {
     let mut v = [5, 12, 7, 40, 35, 26, 5];
     println!("Average: {}", average(&v));
     println!("Median: {}", median(&mut v));
+    println!("Mode: {}", mode(&v));
+}
+
+fn mode(numbers: &[i32]) -> i32 {
+    let mut occurrences = HashMap::new();
+
+    for &value in numbers {
+        *occurrences.entry(value).or_insert(0) += 1;
+    }
+
+    occurrences
+        .into_iter()
+        .max_by_key(|&(_, count)| count)
+        .map(|(val, _)| val)
+        .expect("Cannot compute the mode of zero numbers")
 }
